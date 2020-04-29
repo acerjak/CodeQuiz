e54233f3012cc721668ff6b5a6aa4d73d623d92c
acerjak <62491401+acerjak@users.noreply.github.com>
2020-04-29 00:20:06 -0700

    update time after user feedback "was too short"

--- script2.js
+++ script2.js
@@ -13,7 +13,7 @@ const scoreButton = document.getElementById('score-btn')
 let isOver = false
 score = 0
 //initial time in count down
-let count = 30
+let count = 60
 //time in seconds 
 const timer = document.getElementById('timer')
 //variable for question container
